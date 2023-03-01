2f61c3d1f1674e84ce3664c39dbc0f283e1f3055
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-03-01 23:17:36 +0530

    compnents created based on netflix project

--- projects/shopping/src/app/app.module.ts
+++ projects/shopping/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { netflixRegister } from '../app/components/netflix-register.component';
 import { LoginComponent } from './components/logincomponent/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NetflixHeaderComponent } from './Netflix/netflix-header/netflix-header.component';
+import { NetflixIndexComponent } from './Netflix/netflix-index/netflix-index.component';
+import { NetflixMainComponent } from './Netflix/netflix-main/netflix-main.component';
+import { NetflixRegisterComponent } from './Netflix/netflix-register/netflix-register.component';
 
 @NgModule({
-  declarations: [AppComponent, netflixRegister, LoginComponent],
+  declarations: [
+    AppComponent,
+    netflixRegister,
+    LoginComponent,
+    HomeComponent,
+    NetflixHeaderComponent,
+    NetflixIndexComponent,
+    NetflixMainComponent,
+    NetflixRegisterComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [LoginComponent],
+  bootstrap: [NetflixIndexComponent],
 })
 export class AppModule {}
