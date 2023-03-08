75ac3189ca590387c7cb7d0ff135ad732860c68d
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-03-08 12:22:29 +0530

    ngFor attributes which includes index first last even odd

--- projects/shopping/src/app/components/for-demo/for-demo.component.ts
+++ projects/shopping/src/app/components/for-demo/for-demo.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fakestoreapiContract } from '../../contracts/fakestoreapi';
 
 @Component({
   selector: 'app-for-demo',
   templateUrl: './for-demo.component.html',
   styleUrls: ['./for-demo.component.css'],
 })
-export class ForDemoComponent {
+export class ForDemoComponent implements OnInit {
   // public Categories: string[] = ['All', 'Electronics', 'Footwear', 'Fashion'];
   public Categories: { categories: string; products: string[] }[] = [
     {
@@ -28,4 +29,15 @@ export class ForDemoComponent {
       Price: 73000,
     },
   ];
+  public Product: fakestoreapiContract[] = [];
+  async ngOnInit(): Promise<void> {
+    try {
+      const response = await fetch('http://fakestoreapi.com/products');
+      const data = await response.json();
+      this.Product = data;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
