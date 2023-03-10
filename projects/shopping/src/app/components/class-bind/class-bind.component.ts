a24bb096ebc3ab6b6471f0a205811a616b112bd0
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-03-10 11:32:52 +0530

    Dynamic implemention of ngStyle directive

--- projects/shopping/src/app/components/class-bind/class-bind.component.ts
+++ projects/shopping/src/app/components/class-bind/class-bind.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-class-bind',
   templateUrl: './class-bind.component.html',
-  styleUrls: ['./class-bind.component.css']
+  styleUrls: ['./class-bind.component.css'],
 })
 export class ClassBindComponent {
+  public bgStyle: string = 'red';
+  public textStyle: string = 'white';
+  public padding: string = '0px';
+  public fontSize: string = '10px';
 
+  public styleObject: {
+    'background-color': string;
+    color: string;
+    padding: string;
+    'font-size': any;
+    'text-align': string;
+  } = {
+    'background-color': 'red',
+    color: 'white',
+    padding: '10px',
+    'font-size': '20px',
+    'text-align': 'center',
+  };
+
+  public applyClick() {
+    this.styleObject = {
+      'background-color': this.bgStyle,
+      color: this.textStyle,
+      padding: this.padding + 'px',
+      'text-align': 'center',
+      'font-size': this.fontSize + 'px',
+    };
+  }
+
+  public fontSizes() {
+    this.styleObject = {
+      'background-color': this.bgStyle,
+      color: this.textStyle,
+      padding: this.padding + 'px',
+      'text-align': 'center',
+      'font-size': this.fontSize + 'px',
+    };
+  }
 }
