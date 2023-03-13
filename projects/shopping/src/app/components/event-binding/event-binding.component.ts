bbd23fee9b1b2c26a96011928de23e217e500577
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-03-13 20:08:05 +0530

    mouse events by using images

--- projects/shopping/src/app/components/event-binding/event-binding.component.ts
+++ projects/shopping/src/app/components/event-binding/event-binding.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./event-binding.component.css'],
 })
 export class EventBindingComponent {
+  public img_src: string = '/assets/whiteshoe.png';
+  public styling: { position: string; top: string; left: string } = {
+    position: '',
+    left: '',
+    top: '',
+  };
+
   public InsertClick(e: any): void {
     console.log(e);
     document.write(`
@@ -15,5 +22,29 @@ export class EventBindingComponent {
       X Position    : ${e.clientX} <br>
       Ctrl Key      : ${e.ctrlKey}
       `);
+  }
+
+  public setImages(colorName: string): void {
+    switch (colorName) {
+      case 'green':
+        this.img_src = 'assets/greenshoe.png';
+        break;
+      case 'red':
+        this.img_src = 'assets/redshoe.png';
+        break;
+      case 'blue':
+        this.img_src = 'assets/blueshoe.png';
+        break;
+      default:
+        break;
+    }
+  }
+
+  public mouseMove(e: any): void {
+    this.styling = {
+      position: `fixed`,
+      left: e.clientX + 'px',
+      top: e.clientY + 'px',
+    };
   }
 }
