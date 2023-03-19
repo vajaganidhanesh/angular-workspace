9957f30d81f3e3d049ca987da825eef75b023b75
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-03-19 08:18:00 +0530

    percent Json slice keyValue and  i18nSelect Pipelines are implemented

--- projects/shopping/src/app/components/pipe-demo/pipe-demo.component.ts
+++ projects/shopping/src/app/components/pipe-demo/pipe-demo.component.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pipe-demo',
   templateUrl: './pipe-demo.component.html',
   styleUrls: ['./pipe-demo.component.css'],
 })
-export class PipeDemoComponent {
-  public product: { Name: string; Price: number; Mfd: Date } = {
+export class PipeDemoComponent implements OnInit {
+  public categories: string[] = [
+    'Electronics',
+    'Mens Fashion',
+    'Womens Fashion',
+    'Medicines',
+  ];
+
+  public product: { Name: string; Price: number; Mfd: Date; Rate: number } = {
     Name: 'Samsung Tv',
     Price: 54000,
     Mfd: new Date('2023-03-18'),
+    Rate: 0.245,
   };
 
   public name: string = 'dhanesh';
   public method(name: string) {
     name.bold();
+  }
+
+  public delivery: { product: string; city: string }[] = [
+    { product: 'Samsung', city: 'delhi' },
+    { product: 'Nokia', city: 'Hyderabad' },
+    { product: 'Watch', city: 'Mumbai' },
+    { product: 'Iphone', city: 'Goa' },
+  ];
+
+  public status: any = {
+    delhi: 'Delivery in 2 days',
+    Hyderabad: 'Same day Delivery',
+    Mumbai: 'One day Delivery',
+    other: 'Usually dispatched in 2-3 days',
+  };
+  ngOnInit(): void {
+    console.log(JSON.stringify(this.product));
   }
 }
