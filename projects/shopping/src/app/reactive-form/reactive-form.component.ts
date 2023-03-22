d879e73ba5205e745ae3c7283b83c0e0e155d9d9
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-03-22 20:51:37 +0530

    Reactive Forms implemention

--- projects/shopping/src/app/reactive-form/reactive-form.component.ts
+++ projects/shopping/src/app/reactive-form/reactive-form.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent {
-  public frmRegister = new FormGroup({
-    UserName: new FormControl('Your Name'),
-    Age: new FormControl(0),
-    Mobile: new FormControl(''),
+  // public frmRegister = new FormGroup({
+  //   UserName: new FormControl('Your Name'),
+  //   Age: new FormControl(0),
+  //   Mobile: new FormControl(''),
+  // });
+
+  public formRegister = new FormGroup({
+    username: new FormControl(''),
+    age: new FormControl(0),
+    formVendor: new FormGroup({
+      vendorName: new FormControl(''),
+      vendorRating: new FormControl(0),
+    }),
   });
+
+  get vendorName() {
+    return this.formRegister.get('vendorName');
+  }
+
+  get vendorRating() {
+    return this.formRegister.get('vendorRating');
+  }
 
   public submitClick(e: any) {
     alert(JSON.stringify(e));
